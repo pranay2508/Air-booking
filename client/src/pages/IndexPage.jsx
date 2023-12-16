beff200585dcdfd31a1a19a5a038891ac627a954
pranay2508 <bhatnagarpranay8@gmail.com>
2023-12-16 21:07:30 +0530

    login page added

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,10 +1,3 @@
-import Header from "../Header";
-
-// import { Link } from "react-router-dom";
 export default function IndexPage() {
-    return (
-        <div>
-       <Header/>
-      </div>
-    );
+  return <div>index page here</div>;
 }
