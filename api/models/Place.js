3e9a95176cc90540b7fd8fa7f34ea0632dc9fa0d
pranay2508 <bhatnagarpranay8@gmail.com>
2023-12-23 17:57:00 +0530

    places page

---
+++ api/models/Place.js
@@ -0,0 +1,16 @@
+const { default: mongoose } = require("mongoose");
+const PlaceSchema = new mongoose.Schema({
+    owner: {type:mongoose.Schema.Types.ObjectId,ref:'User'},
+    title:String,
+    address:String,
+    photos: [String],
+    description:String,
+    perks:[String],
+    extraInfo:String,
+    checkIn:Number,
+    checkOut:Number,
+    maxGuests:Number,
+});
+
+const PlaceModel= mongoose.model('Place',PlaceSchema);
+module.exports= PlaceModel;
