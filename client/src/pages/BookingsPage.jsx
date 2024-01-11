8f2d62640221092ca3e0fcb50d6791b059e51457
pranay2508 <bhatnagarpranay8@gmail.com>
2024-01-11 18:40:22 +0530

    BookingsPage

--- client/src/pages/BookingsPage.jsx
+++ client/src/pages/BookingsPage.jsx
@@ -1,26 +1,105 @@
 /* eslint-disable react/jsx-key */
-import { useEffect ,useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import AccountNav from "../AccountNav"
-export default function BookingsPage(){
-    const [bookings ,setBookings] =useState()
-    useEffect(()=>{
-        axios.get('/bookings').then(response => {
-            setBookings(response.data);
-        })
-    },[]);
-    return (
-        <div>
-            <AccountNav/>
-            <div>
-                {bookings?.length > 0 && bookings.map(booking => (
-                    <div>
-                    
-                    {booking.checkIn} = {booking.checkOut}
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+import AccountNav from "../AccountNav";
+import PlaceImg from "./PlaceImg";
+import { differenceInCalendarDays, format } from "date-fns";
+export default function BookingsPage() {
+  const [bookings, setBookings] = useState();
+  useEffect(() => {
+    axios.get("/bookings").then((response) => {
+      setBookings(response.data);
+    });
+  }, []);
+  return (
+    <div>
+      <AccountNav />
+      <div>
+        {bookings?.length > 0 &&
+          bookings.map((booking) => (
+            <div className="flex gap-4 bg-gray-200 rounded-2xl overflow-hidden">
+              <div className="w-48">
+                <PlaceImg place={booking.place} />
+              </div>
+              <div className="py-3 pr-3 grow">
+                <h2 className="text-xl">{booking.place.title}</h2>
+                
+                <div className="text-bold">
+                  <div className="flex gap-1 mb-2 mt-4 text-sm text-gray-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    {differenceInCalendarDays(
+                      new Date(booking.checkOut),
+                      new Date(booking.checkIn)
+                    )} -nights:
 
+                    <div className="flex gap-1 items-center ml-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path d="M12.75 12.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM7.5 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM8.25 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM9.75 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM10.5 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM12.75 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM14.25 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM15 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM16.5 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM15 12.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM16.5 13.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" />
+                      <path
+                        fillRule="evenodd"
+                        d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    {format(new Date(booking.checkIn), "yyyy-MM-dd")}
+                  </div>
+                  &rarr;
+                  <div className="flex gap-1 items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path d="M12.75 12.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM7.5 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM8.25 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM9.75 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM10.5 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM12.75 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM14.25 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM15 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM16.5 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM15 12.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM16.5 13.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" />
+                      <path
+                        fillRule="evenodd"
+                        d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    {format(new Date(booking.checkOut), "yyyy-MM-dd")}
+                  </div>
+                     
+                  </div>
+                  <div className="flex gap-1  ">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="w-8 h-7"
+                    >
+                      <path d="M4.5 3.75a3 3 0 0 0-3 3v.75h21v-.75a3 3 0 0 0-3-3h-15Z" />
+                      <path
+                        fillRule="evenodd"
+                        d="M22.5 9.75h-21v7.5a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3v-7.5Zm-18 3.75a.75.75 0 0 1 .75-.75h6a.75.75 0 0 1 0 1.5h-6a.75.75 0 0 1-.75-.75Zm.75 2.25a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <span className="text-xl"> Total price : ${booking.price}</span>
+                   
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
