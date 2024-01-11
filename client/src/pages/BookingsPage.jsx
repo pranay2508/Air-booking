513e0daa704c2ebab022f74511a547bb6a98a342
pranay2508 <bhatnagarpranay8@gmail.com>
2024-01-11 19:32:40 +0530

    completed

--- client/src/pages/BookingsPage.jsx
+++ client/src/pages/BookingsPage.jsx
@@ -3,8 +3,9 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import AccountNav from "../AccountNav";
 import PlaceImg from "./PlaceImg";
-import { differenceInCalendarDays, format } from "date-fns";
+
 import { Link } from "react-router-dom";
+import BookingDates from "../BookingDates";
 export default function BookingsPage() {
   const [bookings, setBookings] = useState();
   useEffect(() => {
@@ -26,59 +27,7 @@ export default function BookingsPage() {
                 <h2 className="text-xl">{booking.place.title}</h2>
                 
                 <div className="text-bold">
-                  <div className="flex gap-1 mb-2 mt-4 text-sm text-gray-500">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                    {differenceInCalendarDays(
-                      new Date(booking.checkOut),
-                      new Date(booking.checkIn)
-                    )} -nights:
-
-                    <div className="flex gap-1 items-center ml-2">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path d="M12.75 12.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM7.5 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM8.25 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM9.75 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM10.5 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM12.75 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM14.25 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM15 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM16.5 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM15 12.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM16.5 13.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" />
-                      <path
-                        fillRule="evenodd"
-                        d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                    {format(new Date(booking.checkIn), "yyyy-MM-dd")}
-                  </div>
-                  &rarr;
-                  <div className="flex gap-1 items-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path d="M12.75 12.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM7.5 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM8.25 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM9.75 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM10.5 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM12.75 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM14.25 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM15 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM16.5 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM15 12.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM16.5 13.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" />
-                      <path
-                        fillRule="evenodd"
-                        d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                    {format(new Date(booking.checkOut), "yyyy-MM-dd")}
-                  </div>
-                     
-                  </div>
+                  <BookingDates booking={booking} className=" mb-2 mt-4 text-sm text-gray-500"/>
                   <div className="flex gap-1  ">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import BookingWidget from "../BookingWidget";
+import PlaceGallery from "../PlaceGallery";
+import AddressLink from "../AddressLink";
 export default function PlacesPage() {
   const { id } = useParams();
 
   const [place, setPlace] = useState(null);
-  const [showAllPhotos, setShowAllPhotos] = useState(false);
+
   useEffect(() => {
     if (!id) {
       return;
@@ -24,113 +26,13 @@ export default function PlacesPage() {
 
   if (!place) return "";
 
-  if (showAllPhotos) {
-    return (
-      <div className="absolute inset-0 bg-black text-white min-h-screen text-bold">
-        <div className="bg-black p-8 grid gap-4">
-          <div>
-            <h2 className="text-3xl mr-48">Photos of {place.title}</h2>
-            <button
-              onClick={() => setShowAllPhotos(false)}
-              className=" fixed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl shadow-black bg-white text-black"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              Close Photos
-            </button>
-          </div>
-          {place?.photos?.length > 0 &&
-            place.photos.map((photo) => (
-              // eslint-disable-next-line react/jsx-key
-              <div>
-                <img src={"http://localhost:4000/uploads/" + photo} alt="" />
-              </div>
-            ))}
-        </div>
-      </div>
-    );
-  }
   return (
     <div className="mt-4 bg-gray-100 -mx-8 px-8 pt-8">
       <h1 className="text-3xl">{place.title} </h1>
-      <a
-        className="flex gap-1 my-3 block font-semibold underline"
-        target="_blank"
-        href={`https://maps.gooogle.com/?=${place.address}`}
-        rel="noreferrer"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            fillRule="evenodd"
-            d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
-            clipRule="evenodd"
-          />
-        </svg>
+      <AddressLink>{place.address}</AddressLink>
 
-        {place.address}
-      </a>
-      <div className="relative">
-        <div className="grid gap-2  grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
-          <div>
-            {place.photos?.[0] && (
-              <div>
-         
-                <img onClick={()=> setShowAllPhotos(true)} className="cursor-pointer"
-                  src={"http://localhost:4000/uploads/" + place.photos[0]}
-                />
-              </div>
-            )}
-          </div>
-          <div className="grid">
-            {place.photos?.[1] && (
-              <img onClick={()=> setShowAllPhotos(true)} className="cursor-pointer"
-              src={"http://localhost:4000/uploads/" + place.photos[1]} />
-            )}
-            <div className="overflow-hidden">
-              {place.photos?.[2] && (
-                <img
-                  onClick={()=> setShowAllPhotos(true)} 
-                  className="cursor-pointer relative top-2"
-                  src={"http://localhost:4000/uploads/" + place.photos[2]}
-                />
-              )}
-            </div>
-          </div>
-        </div>
-        <button
-          onClick={() => setShowAllPhotos(true)}
-          className="flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              fillRule="evenodd"
-              d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
-              clipRule="evenodd"
-            />
-          </svg>
-          More clicks
-        </button>
-      </div>
+      <PlaceGallery place={place} />
+
       <div className="mt-8 mb-8 grid gap-8 grid-cols-1 sm:grid-cols-[2fr_1fr]">
         <div>
           <div className="my-1">
@@ -148,13 +50,11 @@ export default function PlacesPage() {
         </div>
       </div>
       <div className="bg-white mt-2 -mx-8 px-8 py-8 border-t rounded-xl">
-      <h2 className="font-semibold text-2xl">Extra Info</h2>
-      <div className=" pb-3 mb-4 mt-2 text-sm text-gray-700 leading-5">
-        {place.extraInfo}
+        <h2 className="font-semibold text-2xl">Extra Info</h2>
+        <div className=" pb-3 mb-4 mt-2 text-sm text-gray-700 leading-5">
+          {place.extraInfo}
+        </div>
       </div>
-      </div>
-     
     </div>
   );
 }
-
